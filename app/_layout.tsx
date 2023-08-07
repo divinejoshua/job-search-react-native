4294cd4e016ae03dff7a9380d5f1e53d187121f7
divinejoshua <divineuche399@gmail.com>
2023-08-07 18:13:29 +0100

    Created index page

---
+++ app/_layout.tsx
@@ -0,0 +1,57 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "index",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+         <Stack initialRouteName="index">
+            <Stack.Screen name="index" />
+        </Stack>
+    </ThemeProvider>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from '../components/Themed'
+import { useState } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+
+
+export default function home() {
+
+  const router = useRouter()
+
+  // Data 
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  return (
+    <View>
+      <Text>home</Text>
+    </View>
+  )
+}
