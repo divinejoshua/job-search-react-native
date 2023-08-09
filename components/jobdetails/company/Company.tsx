fc74c119683f228d0ec42eac70fe5da5fd667936
divinejoshua <divineuche399@gmail.com>
2023-08-09 16:45:24 +0100

    Updated company component

--- components/jobdetails/company/Company.tsx
+++ components/jobdetails/company/Company.tsx
@@ -5,11 +5,47 @@ import { icons } from "../../../constants";
 import { checkImageURL } from "../../../utils";
 
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location } : { companyLogo : string, jobTitle : string, companyName : string, location : string}) => {
   return (
-    <View>
-      <Text>Company</Text>
-    </View>
+      <View style={styles.container}>
+
+        {/* Company image  */}
+        <View style={styles.logoBox}>
+          <Image
+            source={{
+              uri: checkImageURL(companyLogo)
+                ? companyLogo
+                : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+            }}
+            style={styles.logoImage}
+          />
+        </View>
+
+        {/* Job title  */}
+        <View style={styles.jobTitleBox}>
+          <Text style={styles.jobTitle}>{jobTitle}</Text>
+        </View>
+
+        {/* Company info  */}
+        <View style={styles.companyInfoBox}>
+
+          {/* Company name  */}
+          <Text style={styles.companyName}>{companyName} / </Text>
+          <View style={styles.locationBox}>
+
+            {/* Location icon  */}
+            <Image
+              source={icons.location}
+              resizeMode='contain'
+              style={styles.locationImage}
+            />
+
+            {/* Company location  */}
+            <Text style={styles.locationName}>{location}</Text>
+          </View>
+        </View>
+      </View>
+
   )
 }
 
