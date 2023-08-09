b04d0a84c042b37e454422bdcca53ac559474729
divinejoshua <divineuche399@gmail.com>
2023-08-09 16:12:31 +0100

    added necessary imports

--- app/job-details/[id].tsx
+++ app/job-details/[id].tsx
@@ -1,5 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hooks/useFetch";
 
 export default function JobDetails() {
   return (
