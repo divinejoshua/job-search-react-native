a0775c84854024cf56ce760bdff37804fb6a5a03
divinejoshua <divineuche399@gmail.com>
2023-08-09 17:03:04 +0100

    Added imports to tabs component

---
+++ components/jobdetails/tabs/Tabs.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { TouchableOpacity, FlatList, Text, View } from "react-native";
+
+import styles from "./tabs.style";
+import { SIZES } from "../../../constants";
+
+
+const Tabs = () => {
+  return (
+    <View>
+      <Text>Tabs </Text>
+    </View>
+  )
+}
+
+export default Tabs
