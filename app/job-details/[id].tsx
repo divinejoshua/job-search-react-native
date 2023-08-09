d82846e7c18ddbd6db8001e9c93fb5acf83b0163
divinejoshua <divineuche399@gmail.com>
2023-08-09 16:36:40 +0100

    Added components to job details page

--- app/job-details/[id].tsx
+++ app/job-details/[id].tsx
@@ -25,16 +25,20 @@ const tabs = ["About", "Qualifications", "Responsibilities"];
 
 
 export default function JobDetails() {
+    
     const params = useLocalSearchParams();
     const router = useRouter();
   
+    // Use fetch hook
     const { data, isLoading, error, refetch } = useFetch("job-details", {
       job_id: params.id,
     });
   
+    // Data 
     const [activeTab, setActiveTab] = useState(tabs[0]);
     const [refreshing, setRefreshing] = useState(false);
   
+    // Refresh funtion 
     const onRefresh = useCallback(() => {
       setRefreshing(true);
       refetch()
@@ -42,9 +46,97 @@ export default function JobDetails() {
     }, []);
 
 
+    // Content to display in tab 
+    const displayTabContent = () => {
+        switch (activeTab) {
+          case "Qualifications":
+            return (
+              <Specifics
+                title='Qualifications'
+                points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+              />
+            );
+    
+          case "About":
+            return (
+              <JobAbout info={data[0].job_description ?? "No data provided"} />
+            );
+    
+          case "Responsibilities":
+            return (
+              <Specifics
+                title='Responsibilities'
+                points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+              />
+            );
+    
+          default:
+            return null;
+        }
+    }
+
+
   return (
-    <View>
-      <Text>JobDetails</Text>
-    </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+
+            {/* Stack screen settings  */}
+            <Stack.Screen
+            options={{
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerBackVisible: false,
+                headerLeft: () => (
+                <ScreenHeaderBtn
+                    iconUrl={icons.left}
+                    dimension='60%'
+                    handlePress={() => router.back()}
+                />
+                ),
+                headerRight: () => (
+                <ScreenHeaderBtn iconUrl={icons.share} dimension='60%' />
+                ),
+                headerTitle: "",
+            }}
+            />
+
+            <>
+                {/* Scroll view  */}
+                <ScrollView showsVerticalScrollIndicator={false}
+                refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+                >
+                {isLoading ? (
+                    <ActivityIndicator size='large' color={COLORS.primary} />
+                ) : error ? (
+                    <Text>Something went wrong</Text>
+                ) : data.length === 0 ? (
+                    <Text>No data available</Text>
+                ) : (
+                    <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+
+                    {/* Company info  */}
+                    <Company
+                        companyLogo={data[0].employer_logo}
+                        jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
+                        location={data[0].job_country}
+                    />
+
+                    {/* Job tabs   */}
+                    <JobTabs
+                        tabs={tabs}
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
+                    />
+
+                    {/* Content of the tab  */}
+                    {displayTabContent()}
+                    </View>
+                )}
+                </ScrollView>
+
+            </>
+
+        </SafeAreaView>
   )
 }
