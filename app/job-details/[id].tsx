2fb52da7f27ced1c523da8dcc472c93965494239
divinejoshua <divineuche399@gmail.com>
2023-08-09 16:14:45 +0100

    Added local search param

--- app/job-details/[id].tsx
+++ app/job-details/[id].tsx
@@ -1,4 +1,4 @@
-import { Stack, useRouter, useSearchParams } from "expo-router";
+import { Stack, useRouter, useLocalSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
 import {
   View,
@@ -20,7 +20,28 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hooks/useFetch";
 
+// Constant variables 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
+
 export default function JobDetails() {
+    const params = useLocalSearchParams();
+    const router = useRouter();
+  
+    const { data, isLoading, error, refetch } = useFetch("job-details", {
+      job_id: params.id,
+    });
+  
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+    const [refreshing, setRefreshing] = useState(false);
+  
+    const onRefresh = useCallback(() => {
+      setRefreshing(true);
+      refetch()
+      setRefreshing(false)
+    }, []);
+
+
   return (
     <View>
       <Text>JobDetails</Text>
